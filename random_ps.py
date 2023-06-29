a4cf7aaab373aebece46c6fc6f5c84f644133cb1
Cbayles <cbayles@outlook.com>
2023-06-29 15:01:43 -0400

    added the random generation loop replaceing numbers & uppercase letters

--- random_ps.py
+++ random_ps.py
@@ -6,3 +6,11 @@ def generate_password(pwd_length):
 
     for i in pwd_length:
         password = ""
+        for j in range(i):
+            next_letter_index = random.randrange(len(alphabet))
+            password = password + alphabet[next_letter_index]
+            #what could be generated 
+        password = replace_with_number(password)
+        password = replace_with_uppercase_letter(password)
+
+        password.append(password)
