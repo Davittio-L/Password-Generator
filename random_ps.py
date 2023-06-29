3eb955ac73cce09d092898549fe1bb15eacdac3a
Cbayles <cbayles@outlook.com>
2023-06-29 15:06:10 -0400

    added 'replace number' function

--- random_ps.py
+++ random_ps.py
@@ -14,3 +14,11 @@ def generate_password(pwd_length):
         password = replace_with_uppercase_letter(password)
 
         password.append(password)
+
+    return passwords
+
+def replace_with_number(pwd_word):
+    for i in range(random.randrange(1, 3)):
+        replace_index = random.randrange(len(pwd_word)//2)
+        pwd_word = pwd_word[0: replace_index] + pwd_word[replace_index].upper() + pwd_word[replace_index + 1]
+        return pwd_word
